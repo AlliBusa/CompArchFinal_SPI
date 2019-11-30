9a468940834d34c82febd2a9abdc5c748c695218
Vienna Scheyer <vscheyer@comcast.net>
2019-11-30 16:01:25 -0500

    Instantiating SPI modules

--- baby.v
+++ baby.v
@@ -4,14 +4,81 @@
 `include "LUT.v"
 
 module baby(
-  input miso,
-  input address,
-  output mosi,
-  output sclk,
-  output cs
+  input MISO,
+  // inputs from whoever is using SPI
+  input SCLK,
+  input CS,
+  input Addr,
+  input Data,
+
+  output MOSI,
+  output SCLK,
+  output CS
 );
- wire 
+
+wire MISOCon, SCLKCon, SCLKPosEdge, SCLKNegEdge, CSCon, DataCon, AddrCon, Sout, SoutDFF;
+wire [7:0] Pin, Pout, PoutData, PoutAddr, AddrCon, DataCon;
+
+inputconditioner MISOinputConditioner (.clk(CLK),
+                                       .noisysignal(MISO),
+                                       .conditioned(MISOCon),
+                                       .positiveedge(MISOPosEdge),
+                                       .negativeedge(MISONegEdge));
+
+inputconditioner SCLKinputConditioner (.clk(CLK),
+                                       .noisysignal(SCLK),
+                                       .conditioned(SCLKCon),
+                                       .positiveedge(SCLKPosEdge),
+                                       .negativeedge(SCLKNegEdge));
+
+inputconditioner CSinputConditioner (.clk(CLK),
+                                       .noisysignal(CS),
+                                       .conditioned(CSCon),
+                                       .positiveedge(CSPosEdge),
+                                       .negativeedge(CSNegEdge));
+
+inputconditioner AddrinputConditioner (.clk(CLK),
+                                       .noisysignal(Addr),
+                                       .conditioned(AddrCon),
+                                       .positiveedge(AddrPosEdge),
+                                       .negativeedge(AddrNegEdge));
+
+inputconditioner AddrinputConditioner (.clk(CLK),
+                                       .noisysignal(Data),
+                                       .conditioned(DataCon),
+                                       .positiveedge(DataPosEdge),
+                                       .negativeedge(DataNegEdge));
+
+shiftregister8 ShiftRegBaby (.parallelOut(Pout),
+                             .clk(clk),
+                             .mode(SRWE),
+                             .parallelIn(Pin),
+                             .serialIn(MOSICon));
+
+datamemory MemBaby (.clk(clk),
+                    .dataOut(Pin),
+                    .address(PoutAddr),
+                    .writeEnable(DMWE),
+                    .dataIn(Pout));
 
 
+// Sout D-Flip Flip
+wire SoutDFF;
+wire Sout;
+registerDFF SoutDFF #(1) (.q(SoutDFF), .d(Sout), .wrenable(SCLKNegEdge), .clk(CLK));
 
-shiftregister8 schwifty ();
+//MOSI BUFF AND GATE
+
+and (SoutBuff, SoutDFF, MOSIBUFF);
+
+// BUFF to CS
+buf (CS, CSCon);
+
+// SCLK BUFF
+
+buf (SCLK, SCLKCon);
+
+// Pout DFF
+registerDFF PoutDFF #(8) (.q(PoutAddrDFF), .d(PoutAddr), .wrenable(AddrWE), .clk(CLK));
+
+// MUX
--- smolboi.v
+++ smolboi.v
@@ -1,15 +1,60 @@
 // Top level module for SPI communication Smol Boi
 
+`include "datamemory.v"
+`include "dflipflop.v"
+
 module SmolBoi (
-  input mosi,
-  input sclk,
-  input cs,
-  output miso
+  input MOSI,
+  input SCLK,
+  input CLK,
+  input CS,
+  output MISO
   );
 
-  //TODO make a clk
+  wire MOSICon, SCLKPosEdge, SCLKNegEdge, CSCon, Sout, SoutDFF, SoutBuff;
+  wire [7:0] Pin, Pout, PoutAddr;
 
-  wire MOSICon, PosEdge, NegEdge, Sout, MISOBuff;
-  wire [7:0] Pin, Pout;
+  // TODO instantiate LUT
 
-  shiftregister8 ShiftRegSmolBoi (.parallelOut(Pout),.clk(clk),.mode(),.parallelIn,.serialIn());
+  inputconditioner MOSIinputConditioner (.clk(CLK),
+                                         .noisysignal(MOSI),
+                                         .conditioned(MOSICon),
+                                         .positiveedge(MOSIPosEdge),
+                                         .negativeedge(MOSINegEdge));
+
+  inputconditioner SCLKinputConditioner (.clk(CLK),
+                                         .noisysignal(SCLK),
+                                         .conditioned(SCLKCon),
+                                         .positiveedge(SCLKPosEdge),
+                                         .negativeedge(SCLKNegEdge));
+
+  inputconditioner CSinputConditioner (.clk(CLK),
+                                         .noisysignal(CS),
+                                         .conditioned(CSCon),
+                                         .positiveedge(CSPosEdge),
+                                         .negativeedge(CSNegEdge));
+
+
+  shiftregister8 ShiftRegSmolBoi (.parallelOut(Pout),
+                                  .clk(clk),
+                                  .mode(SRWE),
+                                  .parallelIn(Pin),
+                                  .serialIn(MOSICon));
+
+  datamemory MemSmolBoi (.clk(clk),
+                         .dataOut(Pin),
+                         .address(PoutAddr),
+                         .writeEnable(DMWE),
+                         .dataIn(Pout));
+
+  register PoutRegSmolBoi (.d(Pout),
+                          .wrenable(AddrWE),
+                          .clk(clk),
+                          .q(PoutAddr));
+
+  register SoutRegSmolBoi (.d(Sout),
+                          .wrenable(clk),
+                          .clk(clk),
+                          .q(SoutDFF));
+
+  and MISOBuffAnd (SoutBuff, SoutDFF, MISOBuff);
