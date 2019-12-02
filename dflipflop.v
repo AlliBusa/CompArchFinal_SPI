13879a6465d6a1eae140bb965d8f752aaac8bf67
Vienna Scheyer <vscheyer@comcast.net>
2019-12-02 00:57:08 -0500

    Trying to run smolboi test bench

--- dflipflop.v
+++ dflipflop.v
@@ -1,11 +1,10 @@
 // Single-bit D Flip-Flop with enable
 //   Positive edge triggered
+
 module registerDFF
-#(parameter width =1),
-)
 (
-output reg	[width-1:0] q,
-input [width-1:0]		d,
+output reg	q,
+input		d,
 input		wrenable,
 input		clk
 );
@@ -17,3 +16,21 @@ input		clk
     end
 
 endmodule
+
+// module registerDFF
+// #(parameter width =1),
+// )
+// (
+// output reg	[width-1:0] q,
+// input [width-1:0]		d,
+// input		wrenable,
+// input		clk
+// );
+//
+//     always @(posedge clk) begin
+//         if(wrenable) begin
+//             q <= d;
+//         end
+//     end
+//
+// endmodule
