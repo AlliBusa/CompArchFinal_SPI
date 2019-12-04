d3e91a68039b88378459a6b0ebdc3e2d8b871c8f
Shashank Swaminathan <sh.swami235@gmail.com>
2019-12-04 18:26:16 +0000

    Updated smol boi to use the LUT

--- smolboi.v
+++ smolboi.v
@@ -4,6 +4,7 @@
 `include "dflipflop.v"
 `include "inputconditioner.v"
 `include "shiftregister.v"
+`include "LUT.v"
 
 module SmolBoi (
   input MOSI,
@@ -11,54 +12,58 @@ module SmolBoi (
   input CLK,
   input CS,
   output MISO
-  );
+);
 
-  wire MOSICon, MOSIPosEdge, MOSINegEdge, SCLKCon, SCLKPosEdge, SCLKNegEdge, CSCon, CSPosEdge, CSNegEdge, Sout, SoutDFF, SoutBuff;
-  wire [7:0] Pin, Pout, PoutAddr;
+   wire  MOSICon, MOSIPosEdge, MOSINegEdge, SCLKCon, SCLKPosEdge, SCLKNegEdge, CSCon, CSPosEdge, CSNegEdge, Sout, SoutDFF, SoutBuff;
+	 wire  AddrWe, DMWE, MISOBuff, SRWE;
+   wire [7:0] Pin, Pout, PoutAddr;
 
-  // TODO instantiate LUT
+   // TODO instantiate LUT
+	 luffy lute(.clk(CLK),.cs(CS),.clkedge(SCLKNegEdge),.sout(Sout),
+							.ADDR_WE(AddrWe),.DM_WE(DMWE),.BUF_E(MISOBuff),.SR_WE(SRWE));
 
-  inputconditioner MOSIinputConditioner (.clk(CLK),
-                                         .noisysignal(MOSI),
-                                         .conditioned(MOSICon),
-                                         .positiveedge(MOSIPosEdge),
-                                         .negativeedge(MOSINegEdge));
+   inputconditioner MOSIinputConditioner (.clk(CLK),
+																					.noisysignal(MOSI),
+																					.conditioned(MOSICon),
+																					.positiveedge(MOSIPosEdge),
+																					.negativeedge(MOSINegEdge));
 
-  inputconditioner SCLKinputConditioner (.clk(CLK),
-                                         .noisysignal(SCLK),
-                                         .conditioned(SCLKCon),
-                                         .positiveedge(SCLKPosEdge),
-                                         .negativeedge(SCLKNegEdge));
+   inputconditioner SCLKinputConditioner (.clk(CLK),
+																					.noisysignal(SCLK),
+																					.conditioned(SCLKCon),
+																					.positiveedge(SCLKPosEdge),
+																					.negativeedge(SCLKNegEdge));
 
-  inputconditioner CSinputConditioner (.clk(CLK),
-                                         .noisysignal(CS),
-                                         .conditioned(CSCon),
-                                         .positiveedge(CSPosEdge),
-                                         .negativeedge(CSNegEdge));
+   inputconditioner CSinputConditioner (.clk(CLK),
+                                        .noisysignal(CS),
+                                        .conditioned(CSCon),
+                                        .positiveedge(CSPosEdge),
+                                        .negativeedge(CSNegEdge));
 
 
-  shiftregister8 ShiftRegSmolBoi (.parallelOut(Pout),
-                                  .clk(CLK),
-                                  .mode(SRWE),
-                                  .parallelIn(Pin),
-                                  .serialIn(MOSICon));
+   shiftregister8 ShiftRegSmolBoi (.parallelOut(Pout),
+                                   .clk(CLK),
+                                   .mode(SRWE),
+                                   .parallelIn(Pin),
+                                   .serialIn(MOSICon),
+																	 .serialOut(Sout));
 
-  datamemory MemSmolBoi (.clk(CLK),
-                         .dataOut(Pin),
-                         .address(PoutAddr),
-                         .writeEnable(DMWE),
-                         .dataIn(Pout));
+   datamemory MemSmolBoi (.clk(CLK),
+													.dataOut(Pin),
+													.address(PoutAddr),
+													.writeEnable(DMWE),
+													.dataIn(Pout));
 
-  registerDFF PoutRegSmolBoi (.d(Pout),
-                          .wrenable(AddrWE),
-                          .clk(CLK),
-                          .q(PoutAddr));
+   registerDFF PoutRegSmolBoi (.d(Pout),
+															 .wrenable(AddrWE),
+															 .clk(CLK),
+															 .q(PoutAddr));
 
-  registerDFF SoutRegSmolBoi (.d(Sout),
-                          .wrenable(CLK),
-                          .clk(CLK),
-                          .q(SoutDFF));
+   registerDFF SoutRegSmolBoi (.d(Sout),
+															 .wrenable(CLK),
+															 .clk(CLK),
+															 .q(SoutDFF));
 
-  and MISOBuffAnd (SoutBuff, SoutDFF, MISOBuff);
+   and MISOBuffAnd (SoutBuff, SoutDFF, MISOBuff);
 
 endmodule
