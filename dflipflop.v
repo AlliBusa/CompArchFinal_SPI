1fff299da2153f2d0e1725e03140b103da12e1c3
Vienna Scheyer <vscheyer@comcast.net>
2019-12-07 18:03:53 -0500

    changed dflipflop to add register for 8 bits

--- dflipflop.v
+++ dflipflop.v
@@ -17,8 +17,26 @@ input		clk
 
 endmodule
 
-// module registerDFF
-// #(parameter width =1),
+module register8
+(
+output reg [7:0] q,
+input       [7:0] d,
+input       wrenable,
+input       clk
+);
+//genvar i;
+//generate
+//for (i=0;i<32;i=i+1)begin
+    always @(posedge clk) begin
+        if(wrenable) begin
+            q<= d;
+        end
+    end
+  //  end
+endmodule
+
+// module registerDFF8
+// #(parameter width =8),
 // )
 // (
 // output reg	[width-1:0] q,
